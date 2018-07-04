5df6aa5786be83774d1eea654e0910f02a5b281b
helikopter14 <40832562+helikopter14@users.noreply.github.com>
2018-07-04 16:23:02 +0200

    Add files via upload

---
+++ main.scala
@@ -0,0 +1,55 @@
+import scala.Array
+import scala.util.Random
+
+object State extends Enumeration {
+    type State = Value
+    val Empty, Mine, Number = Value
+}
+
+class Field (state: State = Empty, value: Int = 0)
+
+class Game (height: Int, width: Int, mines: Int){
+    var board = new Array.ofDim[Field](height, width)
+    
+    def prepareBoard(){
+        var r = new Random
+        var placeForMine
+        for (i <- 1 to mines){
+            do{
+                placeForMine = r.nextInt(weight*width-1)
+            }
+            while(board(placeForMine/height)(placeForMine/width).state!=Empty)
+            x = placeForMine/width
+            y = placeForMine/height
+            board(y)(x).state = Mine
+
+            if(x > 0 && y > 0) board(y-1)(x-1).state += 1
+            if(x > 0) board(y)(x-1).state += 1
+            if(x > 0 && y < height-1) board(y+1)(x-1).state += 1
+            if(y > 0) board(y-1)(x).state += 1
+            if(y < height-1) board(y+1)(x).state += 1
+            if(x < width-1 && y > 0) board(y-1)(x+1).state += 1
+            if(x > width-1) board(y)(x+1).state += 1
+            if(x > width-1 && y < height-1) board(y+1)(x+1).state += 1
+        }
+    }
+
+    def checkField(x: Int, y: Int){
+        board(x)(y) match {
+            case Empty => handleEmpty(x,y)
+            case Bomb => endGame()
+            case Number => showNumber(x,y)
+        }
+    }
+
+}
+
+def printBoard(){
+    for(i <- 0 to height-1){
+        for(j <- 0 to width-1){
+            match board(i)(j).state {
+                case Empty => 
+            }
+        }
+    }
+}
