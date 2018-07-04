16a0ea5d5f92b59a5c84dc575d4420c8ba7b2ae8
helikopter14 <40832562+helikopter14@users.noreply.github.com>
2018-07-04 19:57:16 +0200

    Add files via upload

--- main.scala
+++ main.scala
@@ -1,32 +1,35 @@
-import scala.Array
+import scala.Array._
 import scala.util.Random
 import Console._
+
 
 object State extends Enumeration {
     type State = Value
     val Empty, Mine, Number = Value
 }
 
-class Field (state: State = Empty, value: Int = 0, hidden: Boolean = true)
+import State._
+
+class Field (var state: State = Empty, var value: Int = 0, var hidden: Boolean = true)
 
 class Game (height: Int, width: Int, mines: Int){
-    var board = new Array.ofDim[Field](width, height)
+    var board = ofDim[Field](width, height)
     
     def increaseNeighbour(x: Int, y:Int){
-        if(board(x)(y).state = Empty) board(x)(y).state = Number
+        if(board(x)(y).state == Empty) board(x)(y).state = Number
         board(x)(y).value += 1
     }
 
     def prepareBoard(){
         var r = new Random
-        var placeForMine
+        var placeForMine = 0
         for (i <- 1 to mines){
             do{
-                placeForMine = r.nextInt(weight*width-1)
+                placeForMine = r.nextInt(height*width-1)
             }
             while(board(placeForMine/width)(placeForMine%width).state!=Empty)
-            x = placeForMine/width
-            y = placeForMine%width
+            var x = placeForMine/width
+            var y = placeForMine%width
             board(x)(y).state = Mine
 
             if(x > 0 && y > 0) increaseNeighbour(x-1,y-1)
@@ -41,13 +44,13 @@ class Game (height: Int, width: Int, mines: Int){
     }
 
     def checkField(x: Int, y: Int){
-        if(board(x)(y).hidden = false){
+        if(!board(x)(y).hidden){
             println("Pole juz odsloniete")
 
         }
-        else board(x)(y) match {
+        else board(x)(y).state match {
             case Empty => handleEmpty(x,y)
-            case Bomb => endGame()
+            case Mine => endGame()
             case Number => showNumber(x,y)
         }
     }
@@ -62,23 +65,23 @@ class Game (height: Int, width: Int, mines: Int){
                 if(board(j)(i).hidden==true) println("[ ] ")
                 else board(j)(i).state match{
                     case Empty => println("   ")
-                    case Bomb => println("O ")
+                    case Mine => println("O ")
                     case Number => println(board(j)(i).value+" ")
                 }
             }
         }
     }
 
-    def checkWin():Boolean{
-        Boolean won = true;
+    def checkWin():Boolean = {
+        var won: Boolean = true
         for(i <- 0 to width-1){
             for(j <- 0 to height-1){
-                if(board(i)(j).hidden==true && board(i)(j).state != Bomb) won = false
+                if(board(i)(j).hidden==true && board(i)(j).state != Mine) won = false
             }
         }
         return won
     }
 
-    
+
 }
 
