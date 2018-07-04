ffc679f1d906f1c77eee761014e37d4dbc29bae8
helikopter14 <40832562+helikopter14@users.noreply.github.com>
2018-07-04 17:49:04 +0200

    Add files via upload

--- main.scala
+++ main.scala
@@ -1,5 +1,6 @@
 import scala.Array
 import scala.util.Random
+import Console._
 
 object State extends Enumeration {
     type State = Value
@@ -9,8 +10,13 @@ object State extends Enumeration {
 class Field (state: State = Empty, value: Int = 0, hidden: Boolean = true)
 
 class Game (height: Int, width: Int, mines: Int){
-    var board = new Array.ofDim[Field](height, width)
+    var board = new Array.ofDim[Field](width, height)
     
+    def increaseNeighbour(x: Int, y:Int){
+        if(board(x)(y).state = Empty) board(x)(y).state = Number
+        board(x)(y).value += 1
+    }
+
     def prepareBoard(){
         var r = new Random
         var placeForMine
@@ -18,42 +24,61 @@ class Game (height: Int, width: Int, mines: Int){
             do{
                 placeForMine = r.nextInt(weight*width-1)
             }
-            while(board(placeForMine/height)(placeForMine/width).state!=Empty)
+            while(board(placeForMine/width)(placeForMine%width).state!=Empty)
             x = placeForMine/width
-            y = placeForMine/height
-            board(y)(x).state = Mine
+            y = placeForMine%width
+            board(x)(y).state = Mine
 
-            if(x > 0 && y > 0) board(y-1)(x-1).state += 1
-            if(x > 0) board(y)(x-1).state += 1
-            if(x > 0 && y < height-1) board(y+1)(x-1).state += 1
-            if(y > 0) board(y-1)(x).state += 1
-            if(y < height-1) board(y+1)(x).state += 1
-            if(x < width-1 && y > 0) board(y-1)(x+1).state += 1
-            if(x > width-1) board(y)(x+1).state += 1
-            if(x > width-1 && y < height-1) board(y+1)(x+1).state += 1
+            if(x > 0 && y > 0) increaseNeighbour(x-1,y-1)
+            if(y > 0) increaseNeighbour(x,y-1)
+            if(y > 0 && x < height-1) increaseNeighbour(x+1,y-1)
+            if(x > 0) increaseNeighbour(x-1,y)
+            if(x < height-1) increaseNeighbour(x+1,y)
+            if(y < width-1 && x > 0) increaseNeighbour(x-1,y+1)
+            if(y > width-1) increaseNeighbour(x,y+1)
+            if(y > width-1 && x < height-1) increaseNeighbour(x+1,y+1)
         }
     }
 
     def checkField(x: Int, y: Int){
-        if(board(y)(x).hidden = false){
+        if(board(x)(y).hidden = false){
             println("Pole juz odsloniete")
 
         }
-        else board(y)(x) match {
+        else board(x)(y) match {
             case Empty => handleEmpty(x,y)
             case Bomb => endGame()
             case Number => showNumber(x,y)
         }
     }
 
-}
-
-def printBoard(){
-    for(i <- 0 to height-1){
-        for(j <- 0 to width-1){
-            match board(i)(j).state {
-                case Empty => 
+    def printBoard(){
+        println("   ")
+        for(i <- 0 to width-1) println("("+i+") ")
+        
+        for(i <- 0 to height-1){
+            println("("+i+") ")
+            for(j <- 0 to width-1){
+                if(board(j)(i).hidden==true) println("[ ] ")
+                else board(j)(i).state match{
+                    case Empty => println("   ")
+                    case Bomb => println("O ")
+                    case Number => println(board(j)(i).value+" ")
+                }
             }
         }
     }
+
+    def checkWin():Boolean{
+        Boolean won = true;
+        for(i <- 0 to width-1){
+            for(j <- 0 to height-1){
+                if(board(i)(j).hidden==true && board(i)(j).state != Bomb) won = false
+            }
+        }
+        return won
+    }
+
+    
 }
+
